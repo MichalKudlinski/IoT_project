8b9599e5aa48597d6a226b77f88d3a43c23bbd3c
michal <michal.kudlinski@gmail.com>
2024-07-04 15:39:08 +0200

    fix_commit2

--- library_app/wsgi.py
+++ library_app/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 settings_module = 'library_app.development' if 'WEBSITE_HOSTNAME' in os.environ else 'library_app.development'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_app.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
